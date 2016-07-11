3c108ba117a144d8551df19388fd676273523c47
Myung-jong Kim <mjkim610@gmail.com>
2016-07-11 13:33:14 +0900

    store url, password, time into array objects

--- content.js
+++ content.js
@@ -1,38 +1,48 @@
 //find type=password elements array
-var pass = document.querySelectorAll('input[type=password]');
-pass = pass[0];
-//alert(pass.id);
+var password = document.querySelectorAll('input[type=password]');
+password = password[0];
+//alert(password.id);
 
 //find a form including the password element
-var logform = pass.form;
-//alert(logform.id);
+var loginform = password.form;
+//alert(loginform.id);
 
 //find a type=submit element in the form which is found above
-var sub = logform.querySelectorAll('input[type=submit]');
+var sub = loginform.querySelectorAll('input[type=submit]');
 sub = sub[0];
 //alert(sub.id);
 
 // if submit button is clicked call saveLoginHistory function
 sub.onclick = saveLoginHistory;
 
-
-
-
-
-
 // data is saved in 000003.txt at
 // C:\Users\mjkim\AppData\Local\Google\Chrome\User Data\Default\Local Extension Settings\eilllankfpchokjofpgnhjbfppmhjckh
 function saveLoginHistory() {
 
     // check all elements in the form containing the "password input" element
-    alert("Number of elements in form: " + logform.elements.length);
+    alert("Number of elements in form: " + loginform.elements.length);
     var i;
-    for (i = 0; i < logform.elements.length; i++) {
-        alert("Element " + i + ": " + logform.elements[i].id);
+    for (i = 0; i < loginform.elements.length; i++) {
+        alert("Element " + i + ": " + loginform.elements[i].id);
     }
 
-    // actual storage logic
-    chrome.storage.local.set({'url': document.URL});
-    chrome.storage.local.set({'password': pass.value});
-    chrome.storage.local.set({'time': Date.now()});
+    // store each variable into corresponding arrays
+    chrome.storage.local.get({urls: []}, function (result) {
+        var urls = result.urls;
+        urls.push(document.URL);
+//        chrome.storage.local.remove({ urls: urls });
+        chrome.storage.local.set({ urls: urls });
+    });
+    chrome.storage.local.get({passwords: []}, function (result) {
+        var passwords = result.passwords;
+        passwords.push(password.value);
+//        chrome.storage.local.remove({ passwords: passwords });
+        chrome.storage.local.set({ passwords: passwords });
+    });
+    chrome.storage.local.get({loginTimes: []}, function (result) {
+        var loginTimes = result.loginTimes;
+        loginTimes.push(Date.now());
+//        chrome.storage.local.remove({ loginTimes: loginTimes });
+        chrome.storage.local.set({ loginTimes: loginTimes });
+    });
 }
--- getInfo.js
+++ getInfo.js
@@ -1,17 +1,39 @@
 // write relevant info to home.html
 // currently only reads when 1 entry exists
 // don't know what will happen when there are multiple entries
-chrome.storage.local.get('url', function (urlResult) {
-    url = urlResult.url;
-    document.getElementById("urlInfo").innerHTML = url;
+chrome.storage.local.get({urls: []}, function (result) {
+    var urls = result.urls;
+    document.getElementById('urlInfo').innerHTML = urls[0];
 });
 
-chrome.storage.local.get('password', function (passwordResult) {
-    password = passwordResult.password;
-    document.getElementById("passwordInfo").innerHTML = password;
+chrome.storage.local.get({passwords: []}, function (result) {
+    var passwords = result.passwords;
+    document.getElementById('passwordInfo').innerHTML = passwords[0];
 });
 
-chrome.storage.local.get('time', function (timeResult) {
-    time = timeResult.time;
-    document.getElementById("timeInfo").innerHTML = time;
+chrome.storage.local.get({loginTimes: []}, function (result) {
+    var loginTimes = result.loginTimes;
+    document.getElementById('timeInfo').innerHTML = loginTimes[0];
 });
+
+chrome.storage.local.get(null, function (loginResult) {
+    login = Object.keys(loginResult);
+    document.getElementById("loginInfo").innerHTML = login;
+})
+
+/*
+var tableDiv = document.getElementById("loginInfo");
+var table = document.createElement('TABLE');
+var tableBody = document.createElement('TBODY');
+
+table.border = '1';
+table.appendChild(tableBody);
+
+var heading = new Array();
+heading[0] = "URL";
+heading[1] = "Password";
+heading[2] = "Time";
+
+var log = new Array();
+log[0] = new Array();
+*/
