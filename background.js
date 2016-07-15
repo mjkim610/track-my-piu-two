3a1afac1867ab19e2143317d7c0ef4b95e156929
gyen92 <gyen92@gmail.com>
2016-07-15 00:12:26 -0400

    Add files via upload

--- background.js
+++ background.js
@@ -4,26 +4,37 @@
 function focusOrCreateTab(url) {
     chrome.windows.getAll({"populate":true}, function(windows) {
         var existing_tab = null;
+		var flag = false;
+		//alert("There are "+windows.length+" windows");
         for (var i in windows) {
+			//alert("The "+i+" th window.id is "+windows[i].id);
             var tabs = windows[i].tabs;
+			
             for (var j in tabs) {
                 var tab = tabs[j];
-                if (tab.url == url) {
-                    existing_tab = tab;
+				//alert(tab.url);
+                if (tab.url == url) {					
+                    alert("it's already opened");
+					//chrome.tabs.update(existing_tab.id, {"selected":true});
+					
+					chrome.tabs.reload(tab.id);
+					flag = true;
                     break;
                 }
             }
         }
-        if (existing_tab) {
-            chrome.tabs.update(existing_tab.id, {"selected":true});
-        } else {
+        
+		if (!flag){
+			alert("it's not opened yet");
             chrome.tabs.create({"url":url, "selected":true});
         }
+		
     });
 }
 
 // if extension icon is clicked call the focusOrCreateTab function with the home_url parameter
 chrome.browserAction.onClicked.addListener(function(tab) {
+	
     var home_url = chrome.extension.getURL("home.html");
     focusOrCreateTab(home_url);
 });
