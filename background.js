dd5541f3b3695bff6e7327df85eec6cd2233990c
Myung-jong Kim <mjkim610@gmail.com>
2016-07-15 14:16:01 +0900

    fix background.js so that home.html is focused when button is clicked

--- background.js
+++ background.js
@@ -2,39 +2,30 @@
 // if any of the tabs has the url open, focus to that tab
 // else open a new tab with the url
 function focusOrCreateTab(url) {
-    chrome.windows.getAll({"populate":true}, function(windows) {
-        var existing_tab = null;
-		var flag = false;
-		//alert("There are "+windows.length+" windows");
+    chrome.windows.getAll({ "populate": true }, function(windows) {
+        var pageIsOpen = false;
         for (var i in windows) {
-			//alert("The "+i+" th window.id is "+windows[i].id);
             var tabs = windows[i].tabs;
-			
             for (var j in tabs) {
                 var tab = tabs[j];
-				//alert(tab.url);
-                if (tab.url == url) {					
-                    alert("it's already opened");
-					//chrome.tabs.update(existing_tab.id, {"selected":true});
-					
-					chrome.tabs.reload(tab.id);
-					flag = true;
+                if (tab.url == url) {
+                    chrome.tabs.reload(tab.id);
+                    chrome.tabs.update(tab.id, { "selected": true });
+                    chrome.windows.update(windows[i].id, { "focused": true });
+                    pageIsOpen = true;
                     break;
                 }
             }
         }
-        
-		if (!flag){
-			alert("it's not opened yet");
-            chrome.tabs.create({"url":url, "selected":true});
+
+        if (!pageIsOpen) {
+            chrome.tabs.create({ "url": url, "selected": true });
         }
-		
     });
 }
 
 // if extension icon is clicked call the focusOrCreateTab function with the home_url parameter
 chrome.browserAction.onClicked.addListener(function(tab) {
-	
     var home_url = chrome.extension.getURL("home.html");
     focusOrCreateTab(home_url);
 });
