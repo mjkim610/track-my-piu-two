8f47c25fbe4f389e025ed42d47fb449394a9579c
Myung-jong Kim <mjkim610@gmail.com>
2016-07-19 13:51:08 +0900

    count number of matching results at home.html

--- home.js
+++ home.js
@@ -4,7 +4,7 @@ function loadTable() {
     chrome.storage.local.get(null, function (result) {
         entryCount = result.urls.length;
 
-        var row, cellUrl, cellUsername, cellPassword, cellTime, cellAttempt,
+        var row, cellUrl, cellUsername, cellPassword, cellTime,
             timeConverted, yyyy, mm, dd, hh, minute, ss, ampm;
 
         // insert each entry into a new row
@@ -41,42 +41,55 @@ function loadTable() {
         }
     });
 }
-
 function showResult() {
     if (searchText.value == "") {
         alert("SearchBox is Empty!");
     }
     else {
-		rows = table.getElementsByTagName("tr");
-			if (radioURL.checked) {
-				alert("search by URL");
-				for(var i = 1; i<rows.length; i++){
-					var row = rows[i];
-					var url = row.getElementsByTagName("td")[0].innerHTML;
+        rows = table.getElementsByTagName("tr");
+        var count = 0;
+        if (radioURL.checked) {
+            alert("search by URL");
+            for (var i = 1; i < rows.length; i++) {
+                var row = rows[i];
+                var url = row.getElementsByTagName("td")[0].innerHTML;
+                if (url.includes(searchText.value)) {
+                    row.style.display = '';
+                    count++;
+                }
+                else {
+                    row.style.display = 'none';
+                }
+            }
+            countResult(count);
+        }
+        else if (radioID.checked) {
+            alert("search by UserName");
+            for (var i = 1; i < rows.length; i++) {
+                var row = rows[i];
+                var userName = row.getElementsByTagName("td")[1].innerHTML;
+                if (userName.includes(searchText.value)) {
+                    row.style.display = '';
+                    count++;
+                }
+                else {
+                    row.style.display = 'none';
+                }
+            }
+            countResult(count);
+        }
+        else {
+            alert("Please select search type!");
+        }
+    }
+}
 
-					if(url.includes(searchText.value)){
-						row.style.display='';
-					}else{
-						row.style.display='none';
-					}
-				}
-			}else if (radioID.checked) {
-				alert("search by ID");
-				for(var i = 1; i<rows.length; i++){
-					var row = rows[i];
-					var userName = row.getElementsByTagName("td")[1].innerHTML;
-
-					if(userName.includes(searchText.value)){
-						row.style.display='';
-					}else{
-						row.style.display='none';
-					}
-				}
-			}
-			else {
-				alert("Please select search type!");
-			}
-
+function countResult(count) {
+    if (count != 0) {
+        alert("There are " + count + " matching result(s).");
+    }
+    else {
+        alert("There are no matching result.");
     }
 }
 
@@ -85,19 +98,14 @@ function resetHistory() {
     alert("History cleared!");
     chrome.tabs.reload();
 }
-function removeTable(){
-	for(i=0; i<entryCount; i++){
-		row = table.deleteRow(entryCount-i);
-	}
-}
-var entryCount;
-var resetButton = document.querySelector("button[id=reset]");
-var searchButton = document.querySelector("button[id=search]");
-var searchText = document.getElementById("searchText");
-var table = document.getElementById("loginInfoTable");
-var radioURL = document.getElementById("radioURL");
-var radioID = document.getElementById("radioID");
 
+function removeTable() {
+    for (i = 0; i < entryCount; i++) {
+        row = table.deleteRow(entryCount - i);
+    }
+}
+
+var entryCount;
 var resetButton = document.querySelector("button[id=reset]");
 var searchButton = document.querySelector("button[id=search]");
 var searchText = document.getElementById("searchText");
