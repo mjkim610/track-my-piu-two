68d822f0753207a9c65dcea99567a5c03616079f
gyen92 <gyen92@gmail.com>
2016-07-25 03:25:44 -0400

    search by url/username and by time by two flags

--- home.js
+++ home.js
@@ -10,11 +10,13 @@ function loadTable() {
         // insert each entry into a new row
         for (i=0; i<entryCount; i++) {
             row = table.insertRow(i+1);
-            cellUrl = row.insertCell(0);
+			cellUrl = row.insertCell(0);
             cellUsername = row.insertCell(1);
             cellPassword = row.insertCell(2);
             cellTime = row.insertCell(3);
             cellAttempt = row.insertCell(4);
+			cellSFlag = row.insertCell(5);
+			cellTFlag = row.insertCell(6);
 
             // format time variable into readable format
             timeConverted = new Date(result.times[i]);
@@ -38,6 +40,10 @@ function loadTable() {
             cellPassword.innerHTML = result.passwords[i];
             cellTime.innerHTML = timeConverted;
             cellAttempt.innerHTML = result.attempts[i];
+			cellSFlag.innerHTML = "T";
+			//cellSFlag.style.display='none';
+			cellTFlag.innerHTML = "T";
+			//cellTFlag.style.display='none';
         }
     });
 }
@@ -50,21 +56,19 @@ function showResult() {
 	//if there is text to search
     else {
         rows = table.getElementsByTagName("tr");
-        var count = 0;
         if (radioURL.checked) {
             alert("search by URL");
             for (var i = 1; i < rows.length; i++) {
                 var row = rows[i];
                 var url = row.getElementsByTagName("td")[0].innerHTML;
                 if (url.includes(searchText.value)) {
-                    row.style.display = '';
-                    count++;
+                    row.getElementsByTagName("td")[5].innerHTML="T";
                 }
                 else {
-                    row.style.display = 'none';
+                    row.getElementsByTagName("td")[5].innerHTML="F";
                 }
             }
-            countResult(count);
+            show();
         }
         else if (radioID.checked) {
             alert("search by UserName");
@@ -72,14 +76,13 @@ function showResult() {
                 var row = rows[i];
                 var userName = row.getElementsByTagName("td")[1].innerHTML;
                 if (userName.includes(searchText.value)) {
-                    row.style.display = '';
-                    count++;
+                    row.getElementsByTagName("td")[5].innerHTML="T";
                 }
                 else {
-                    row.style.display = 'none';
+                    row.getElementsByTagName("td")[5].innerHTML="F";
                 }
             }
-            countResult(count);
+            show();
         }
         else {
             alert("Please select search type!");
@@ -110,54 +113,143 @@ function removeTable() {
 
 function changeSelect(){
 	var selectedIndex = dropdown.selectedIndex;
+	//if (tempTable!=null){
+		//table = tempTable;}
 	var rows = table.getElementsByTagName("tr");
 	
 	var time = new Date();
-	var year = time.getFullYear();//alert("year: "+year);
-	var month = time.getMonth()+1;//alert("month: "+month);
-	var date = time.getDate();//alert("date: "+date);
-	var todayT = dateToInt(year, month, date);
+	var today = dateToInt(time.getFullYear(), time.getMonth()+1, time.getDate());
+	//alert("today: "+today);
+
 	
-	var yesterday = time.setDate(time.getDate()-1);
 	
-	var yearY = yesterday.getFullYear(); 
-	var monthY = yesterday.getMonth()+1;
-	var dateY = yesterday.getDate();
-	yesterday = dateToInt(yearY, monthY, dateY);
-	alert(yesterday);
+	var twodayago = new Date();
+	twodayago.setDate(twodayago.getDate()-2);
+	twodayago = dateToInt(twodayago.getFullYear(), twodayago.getMonth()+1, twodayago.getDate());
+	//alert(twodayago);
 	
 	switch(selectedIndex) {
 		
 		case 0://whole
-			alert(dropdown.options[0].value);
+			alert(dropdown.options[0].value)
 			for (var i = 1; i < rows.length; i++){
 				var row = rows[i];
-				var timeC = row.getElementsByTagName("td")[3].innerHTML;
-				var yearC = getByIndex(timeC, 0, 3);
-				var monthC = getByIndex(timeC, 5, 6);
-				var dateC = getByIndex(timeC, 8, 9);
-				var then = dateToInt(yearC,monthC,dateC);
-				
+				row.getElementsByTagName("td")[6].innerHTML ="T";
 			}
+			
+			
 			break;
 		case 1://1day
-			alert(dropdown.options[1].value);
-			break;
+			alert(dropdown.options[1].value);			
+	
+			for (var i = 1; i < rows.length; i++){
+				var row = rows[i];
+				if (row.style.display!='none'){
+					var timeC = row.getElementsByTagName("td")[3].innerHTML;
+					var yearC = getByIndex(timeC, 0, 3);
+					var monthC = getByIndex(timeC, 5, 6);
+					var dateC = getByIndex(timeC, 8, 9);
+					var then = dateToInt(yearC,monthC,dateC);
+					//alert(then);
+					if(then!=today){
+						row.getElementsByTagName("td")[6].innerHTML="F";
+					}else{
+						row.getElementsByTagName("td")[6].innerHTML="T";
+					}
+				}
+			}break;
 		case 2://2days
-			alert(dropdown.options[2].value);	
-			break;
+			alert(dropdown.options[2].value);			
+			var yesterday = new Date();
+			yesterday.setDate(yesterday.getDate() -1);
+			yesterday = dateToInt(yesterday.getFullYear(), yesterday.getMonth()+1, yesterday.getDate());
+			//alert(yesterday);
+			for (var i = 1; i < rows.length; i++){
+				var row = rows[i];
+				if (row.style.display!='none'){
+					var timeC = row.getElementsByTagName("td")[3].innerHTML;
+					var yearC = getByIndex(timeC, 0, 3);
+					var monthC = getByIndex(timeC, 5, 6);
+					var dateC = getByIndex(timeC, 8, 9);
+					var then = dateToInt(yearC,monthC,dateC);
+					//alert(then);
+					if((then!=yesterday)&&(then!=today)){
+						row.getElementsByTagName("td")[6].innerHTML="F";
+					}else{
+						row.getElementsByTagName("td")[6].innerHTML="T";
+					}
+				}
+			}break;
 		case 3://1week
 			alert(dropdown.options[3].value);	
-			break;
+			var weekago = new Date();
+			weekago.setDate(weekago.getDate() -6);
+			weekago = dateToInt(weekago.getFullYear(), weekago.getMonth()+1, weekago.getDate());
+			//alert(weekago);
+			for (var i = 1; i < rows.length; i++){
+				var row = rows[i];
+				if (row.style.display!='none'){
+					var timeC = row.getElementsByTagName("td")[3].innerHTML;
+					var yearC = getByIndex(timeC, 0, 3);
+					var monthC = getByIndex(timeC, 5, 6);
+					var dateC = getByIndex(timeC, 8, 9);
+					var then = dateToInt(yearC,monthC,dateC);
+					//alert(then);
+					if((then<weekago)||(then>today)){
+						row.getElementsByTagName("td")[6].innerHTML="F";
+					}else{
+						row.getElementsByTagName("td")[6].innerHTML="T";
+					}
+				}
+			}break;
 		case 4://1month
 			alert(dropdown.options[4].value);	
-			break;
+			var monthago = new Date();
+			monthago.setMonth(monthago.getMonth() -1);
+			monthago = dateToInt(monthago.getFullYear(), monthago.getMonth()+1, monthago.getDate());
+			//alert(monthago);
+			for (var i = 1; i < rows.length; i++){
+				var row = rows[i];
+				if (row.style.display!='none'){
+					var timeC = row.getElementsByTagName("td")[3].innerHTML;
+					var yearC = getByIndex(timeC, 0, 3);
+					var monthC = getByIndex(timeC, 5, 6);
+					var dateC = getByIndex(timeC, 8, 9);
+					var then = dateToInt(yearC,monthC,dateC);
+					//alert(then);
+					if((then<monthago)||(then>today)){
+						row.getElementsByTagName("td")[6].innerHTML="F";
+					}else{
+						row.getElementsByTagName("td")[6].innerHTML="T";
+					}
+				}
+			}break;
 		case 5://1year
 			alert(dropdown.options[5].value);
-			break;
+			var yearago = new Date();
+			yearago.setFullYear(yearago.getFullYear() -1);
+			yearago = dateToInt(yearago.getFullYear(), yearago.getMonth()+1, yearago.getDate());
+			//alert(yearago);
+			for (var i = 1; i < rows.length; i++){
+				var row = rows[i];
+				if (row.style.display!='none'){
+					var timeC = row.getElementsByTagName("td")[3].innerHTML;
+					var yearC = getByIndex(timeC, 0, 3);
+					var monthC = getByIndex(timeC, 5, 6);
+					var dateC = getByIndex(timeC, 8, 9);
+					var then = dateToInt(yearC,monthC,dateC);
+					//alert(then);
+					if((then<yearago)||(then>today)){
+						row.getElementsByTagName("td")[6].innerHTML="F";
+					}else{
+						row.getElementsByTagName("td")[6].innerHTML="T";
+					}
+				}
+			}break;
 		default:
 			break;
 	}
+	show();
 }
 function getByIndex(str, start, finish){
 	var result = "";
@@ -169,6 +261,21 @@ function dateToInt(a, b, c){
 	return parseInt(a)*10000+parseInt(b)*100+parseInt(c);
 	
 }
+function show(){
+	rows = table.getElementsByTagName("tr");
+	var count = 0;
+	for (var i = 1; i<rows.length; i++){
+		var row = rows[i];
+		var SFlag = row.getElementsByTagName("td")[5].innerHTML;
+		var TFlag = row.getElementsByTagName("td")[6].innerHTML;
+		if((SFlag=="T")&&(TFlag=="T")){
+			row.style.display='';
+			count++;
+		}else{
+			row.style.display='none';
+		}
+	}countResult(count);
+}
 
 var entryCount;
 var resetButton = document.querySelector("button[id=reset]");
@@ -178,6 +285,7 @@ var table = document.getElementById("loginInfoTable");
 var radioURL = document.getElementById("radioURL");
 var radioID = document.getElementById("radioID");
 var dropdown = document.getElementById("select");
+
 
 document.body.onload = loadTable;
 resetButton.onclick = resetHistory;
