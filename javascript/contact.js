739d9b2a6fcf1c0dd26a5d59dd0c090afd143e5c
Myung-jong Kim <mjkim610@gmail.com>
2016-09-02 18:39:48 +0900

    add map in contact page

--- javascript/contact.js
+++ javascript/contact.js
@@ -1,3 +1,41 @@
+function initialize() {
+    loadMapsScript();
+    validateForm();
+}
+
+function loadMapsScript() {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAbqw9jWAQB-k-iHkFWM-BoqVPLgjBAgk8&callback=initMap';
+    document.body.appendChild(script);
+}
+
+function initMap() {
+    var map;
+    var yonseiERC = { lat: 37.560847, lng: 126.935469 };
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: yonseiERC,
+        zoom: 17
+    });
+
+    var marker = new google.maps.Marker({
+        position: yonseiERC,
+        animation: google.maps.Animation.DROP,
+        map: map,
+        title: 'Yonsei University Engineering Research Park'
+    });
+
+    var infoWindow = new google.maps.InfoWindow({
+      content: "<h1 class='mapInfo'><b>Eagle Team Headquarters:</b></br>Yonsei University Engineering Research Park</h1>"
+    });
+
+
+    marker.addListener('click', function() {
+      infoWindow.open(map, marker);
+    });
+}
+
 function validateForm() {
     document.getElementById("contactForm").onsubmit = function() {
         var submit = true;
@@ -29,14 +67,21 @@ function validateForm() {
     document.getElementById("message").onkeyup = removeWarning;
 }
 
+function removeWarning() {
+    document.getElementById(this.id + "Error").innerHTML = "";
+}
+
+
+document.body.onload = initialize;
+
+/* ********************
+ * *******NOTES********
+ * ********************
+ */
+// google maps API key: AIzaSyAbqw9jWAQB-k-iHkFWM-BoqVPLgjBAgk8
+
 // utilize this regular expression checker later!
 function validateEmail(email) {
   var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email);
 }
-
-function removeWarning() {
-    document.getElementById(this.id + "Error").innerHTML = "";
-}
-
-document.body.onload = validateForm;
